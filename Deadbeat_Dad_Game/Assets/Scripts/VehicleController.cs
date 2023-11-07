9f421f696cb1ec543fcef86dc71dfe9ffaccaff1
Maisie Palmer <maisiejpalmer@gmail.com>
2023-11-07 20:47:32 +0000

    Vehicles yield

--- Deadbeat_Dad_Game/Assets/Scripts/VehicleController.cs
+++ Deadbeat_Dad_Game/Assets/Scripts/VehicleController.cs
@@ -6,16 +6,66 @@ public class VehicleController : MonoBehaviour
 {
     private Rigidbody controller;
     public float speed = 0f;
+    private Vector3 initPos;
 
     private void Start()
     {
         controller = GetComponent<Rigidbody>();
+        initPos = gameObject.transform.position;
     }
 
     void Update()
     {
         Vector3 move = controller.transform.forward.normalized;
         move.y = 0;
-        gameObject.transform.Translate(move * speed, Space.World);
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 20))
+        {
+            if(hit.collider.CompareTag("Vehicle"))
+            {
+                gameObject.transform.Translate(Vector3.zero, Space.World);
+            }
+            else
+            {
+                gameObject.transform.Translate(move * speed, Space.World);
+            }
+        }
+        else
+        {
+            gameObject.transform.Translate(move * speed, Space.World);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("EndOfRoad"))
+            Respawn();
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Vehicle"))
+        {
+            Respawn();
+        }
+        else if (collision.collider.CompareTag("Intersection"))
+        {
+            WaitForAllClear();
+        }
+    }
+
+    private void Respawn()
+    {
+        // randomly pick from a table of road starts
+        transform.position = initPos;
+        controller.velocity = Vector3.zero;
+    }
+
+    private void WaitForAllClear()
+    {
+        
     }
 }
