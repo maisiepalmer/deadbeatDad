2737d5d5c70d5347ee8573c4d39906ad297c3a5e
Maisie Palmer <maisiejpalmer@gmail.com>
2023-11-07 18:25:20 +0000

    Player controller working

--- Deadbeat_Dad_Game/Assets/Scripts/PlayerController.cs
+++ Deadbeat_Dad_Game/Assets/Scripts/PlayerController.cs
@@ -5,29 +5,62 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 0.1f;
+    public float moveSpeed = 5f;
     private Rigidbody playerController;
-    public Vector3 movement;
 
-    private bool isCollided;
+    Vector3 rotation = Vector3.zero;
+    public float lookSpeed = 10f;
+
+    private Vector3 lastValue;
 
     void Start()
     {
         playerController = GetComponent<Rigidbody>();
+        playerController.useGravity = true;
     }
 
     private void Update()
     {
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * lookSpeed;
     }
 
     private void FixedUpdate()
     {
-        playerController.MovePosition(transform.position + (movement * speed));
+        if (Keyboard.KeyCount > 0)
+            MovePlayerRelativeToCamera();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collided");
+    }
+
+    // ADAPTED FROM: https://youtu.be/7kGCrq1cJew?si=hDGWI_3S8jt2mnoo
+    private void MovePlayerRelativeToCamera()
+    {
+        // get player input
+        float playerVerticalInput = Input.GetAxis("Vertical");
+        float playerHorizontalInput = Input.GetAxis("Horizontal");
+
+        // get camera-normalised directional vectors
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward = forward.normalized;
+        right = right.normalized;
+
+        // create direction-relative input vectors
+        Vector3 forwardVerticalInput = playerVerticalInput * forward;
+        Vector3 rightVerticalInput = playerHorizontalInput * right;
+
+        // create camera-relative movement
+        Vector3 cameraRelativeMovement = (forwardVerticalInput + rightVerticalInput) * moveSpeed;
+
+        // apply to player with smoothing
+        playerController.velocity = (cameraRelativeMovement * 0.9f) + (lastValue * 0.1f);
+        lastValue = cameraRelativeMovement;
     }
 }
