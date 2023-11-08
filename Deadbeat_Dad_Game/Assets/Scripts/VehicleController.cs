4cbc15527d1e899bffa3a1a69cc0f89f4fda59d8
Maisie Palmer <maisiejpalmer@gmail.com>
2023-11-08 12:42:18 +0000

    Vehicle respawn points added

--- Deadbeat_Dad_Game/Assets/Scripts/VehicleController.cs
+++ Deadbeat_Dad_Game/Assets/Scripts/VehicleController.cs
@@ -6,43 +6,23 @@ public class VehicleController : MonoBehaviour
 {
     private Rigidbody controller;
     public float speed = 0f;
-    private Vector3 initPos;
+    public GameObject[] respawnPoints;
 
     private void Start()
     {
         controller = GetComponent<Rigidbody>();
-        initPos = gameObject.transform.position;
     }
 
     void Update()
     {
-        Vector3 move = controller.transform.forward.normalized;
-        move.y = 0;
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 20))
-        {
-            if(hit.collider.CompareTag("Vehicle"))
-            {
-                gameObject.transform.Translate(Vector3.zero, Space.World);
-            }
-            else
-            {
-                gameObject.transform.Translate(move * speed, Space.World);
-            }
-        }
-        else
-        {
-            gameObject.transform.Translate(move * speed, Space.World);
-        }
+        Vector3 frameMovement = CheckAndGetMovement();
+        gameObject.transform.Translate(frameMovement, Space.World);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("EndOfRoad"))
+        if (other.CompareTag("EndOfRoad"))
             Respawn();
-        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -51,21 +31,31 @@ public class VehicleController : MonoBehaviour
         {
             Respawn();
         }
-        else if (collision.collider.CompareTag("Intersection"))
-        {
-            WaitForAllClear();
-        }
     }
 
     private void Respawn()
     {
         // randomly pick from a table of road starts
-        transform.position = initPos;
+        int randZone = Random.Range(0, respawnPoints.Length);
+
+        transform.position = respawnPoints[randZone].transform.position;
+        transform.rotation = respawnPoints[randZone].transform.rotation;
+
         controller.velocity = Vector3.zero;
     }
 
-    private void WaitForAllClear()
+    private Vector3 CheckAndGetMovement()
     {
-        
+        // calculate the movement translation (forward vector multiplied by the speed offset)
+        Vector3 movement = controller.transform.forward.normalized * speed * Time.deltaTime;
+        movement.y = 0;
+
+        /* If the raycast hits a collider
+         - Check if it's a vehicle, if so return a zero vector (no movement)
+         - Otherwise, return the movement translation */
+        RaycastHit hit;
+        return (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 20)) ?
+                                                                                                            (hit.collider.CompareTag("Vehicle")) ? Vector3.zero : movement
+                                                                                                                 : movement;
     }
 }
